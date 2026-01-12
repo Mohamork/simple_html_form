77b91f8851f9db0b613eb4b37f7d63d44a78be72
Mohamork <k.m19981102@gmail.com>
2026-01-12 10:05:56 +0100

    Summary
    - rename the form action to result.html in color.html
    - add path to result page in urls.py
    - rename path from 'yourfavorite-color' to 'result' in urls.py

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.get_color,name='your-favorite-color')
+    path('',views.get_color,name='get the color'),
+    path('result',views.result,name='result')
 ]
