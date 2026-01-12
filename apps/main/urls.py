c02b45645c441bb7da8a66dc8c72318954dea635
Mohamork <k.m19981102@gmail.com>
2026-01-12 10:56:50 +0100

    Summary
    - fix spelling in views.result
    - fix spelling in

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.get_color,name='get the color'),
+    path('',views.get_color,name='favorite_color'),
     path('result',views.result,name='result')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -7,7 +7,13 @@ def get_color(request) :
     if request.method == 'POST' :
         form = ColorForm(request.POST)
         if form.is_valid() :
+            color = form.cleaned_data['favorite_color']
+            request.session['favorite_color'] = color
             return HttpResponseRedirect('/result')
     else:
         form = ColorForm()
-    return render(request,'color.html',{'form':form})        
+    return render(request,'color.html',{'form':form})    
+
+def result(request) :
+    color = request.session.get('favorite_color')
+    return render(request,'result.html',{'color':color})    
