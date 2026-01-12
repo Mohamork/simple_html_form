1a1df7f5e2d971fac1f6e4b756512f47f9763fa7
Mohamork <k.m19981102@gmail.com>
2026-01-12 10:57:01 +0100

    Revert "Summary"
    This reverts commit c02b45645c441bb7da8a66dc8c72318954dea635.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.get_color,name='favorite_color'),
+    path('',views.get_color,name='get the color'),
     path('result',views.result,name='result')
 ]
